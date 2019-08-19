4d42a88c7878946f9dc5cc15198e91815224169a
Rahul Sha <rahul.sha@cisinlabs.com>
2019-08-19 15:46:14 +0530

    Validtion changes on customers and category

--- OnRamp/assets/knockoutjs/KOCreateCategory.js
+++ OnRamp/assets/knockoutjs/KOCreateCategory.js
@@ -1,35 +1,84 @@
 ﻿
 
 $(function () {
-    ko.applyBindings(modelCreateCategory);
+
+    ko.validation.init({
+        errorElementClass: "wrong-field",
+        decorateElement: true,
+        errorClass: 'wrong-field'
+
+    }, true);
+
+    var modelCreateCategory = function () {
+        var self = this;
+        self.validateNow = ko.observable(false);
+        self.Category_ID = ko.observable(),
+        self.Category_Name = ko.observable().extend({
+            required: {
+                message: "category name is required",
+                onlyIf: function () {
+                    return self.validateNow();
+                }
+            }
+        });
+        self.Category_Description = ko.observable().extend({
+            required: {
+                message: "category description is required",
+                onlyIf: function () {
+                    return self.validateNow();
+                }
+            }
+        });
+
+            self.createCategory = function () {
+                self.validateNow(true);
+                $('div.alert-danger').hide();
+                if (self.errors().length === 0) {
+                    try {
+                        $.ajax({
+                            url: '/Category/AddCategory',
+                            type: 'post',
+                            dataType: 'json',
+                            data: ko.toJSON(this), //Here the data wil be converted to JSON
+                            contentType: 'application/json',
+                            success: successCallback,
+                            error: errorCallback
+                        });
+                    } catch (e) {
+                        window.location.href = '/Category/Index/';
+                    }
+                } else {
+                    self.errors.showAllMessages();
+                    $('div.alert-danger').show();
+                    //alert('Please enter category name');
+
+                }
+
+
+            };
+        self.errors = ko.validation.group(self);
+    };
+
+    var viewModel = new modelCreateCategory();
+    ko.applyBindings(viewModel);
+
 });
 
-var modelCreateCategory = {
-    Category_ID: ko.observable(),
-    Category_Name: ko.observable(),
-    Category_Description: ko.observable(),
-    createCategory: function () {
-        try {
-            $.ajax({
-                url: '/Category/AddCategory',
-                type: 'post',
-                dataType: 'json',
-                data: ko.toJSON(this), //Here the data wil be converted to JSON
-                contentType: 'application/json',
-                success: successCallback,
-                error: errorCallback
-            });
-        } catch (e) {
-            window.location.href = '/Category/Index/';
-        }
-    }
-};
 function successCallback(data) {
     bootbox.alert("Category Added successfully!");
     window.location.href = '/Category/Index/';
 }
 function errorCallback(err) {
     window.location.href = '/Category/Index/';
-    
-    
+
+
 }
+
+
+
+
+
+
+
+
+
